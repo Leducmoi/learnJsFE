4b04cb580f83298fa1216f8f323506b4b542cfc7
Le Duc Moi <leducmoi@dtu.edu.vn>
2026-03-30 22:54:57 +0700

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="py-5 bg-black">
+  <div className="container px-5"><p className="m-0 text-center text-white small">Copyright © Your Website 2023</p></div>
+</footer>
+  )
+}
