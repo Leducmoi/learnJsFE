809c424576bf6f2ab4c3f948c434e2dccfecf5da
Le Duc Moi <leducmoi@dtu.edu.vn>
2026-03-31 23:52:27 +0700

    do homework

--- src/App.js
+++ src/App.js
@@ -9,9 +9,25 @@ function App() {
     <div>
       <Nav />
       <Header />
-      <Section img="assets/img/01.jpg" />
-      <Section img="assets/img/02.jpg"/>
-      <Section img="assets/img/03.jpg"/>
+      <Section img="assets/img/01.jpg"
+               title = "For those about to rock..."
+               detail={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid,
+                        mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem,
+                        ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae 
+                        obcaecati.`} />
+      <Section img="assets/img/02.jpg" 
+               title = "For those about to rock..."
+               imagePosition="left"
+               detail={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid,
+                        mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem,
+                        ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae 
+                        obcaecati.`} />
+      <Section img="assets/img/03.jpg"
+               title = "For those about to rock..."
+               detail={`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid,
+                        mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem,
+                        ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae 
+                        obcaecati.`}/>
       <Footer />
     </div>
   );
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 
 export const Section = (props) => {
+    const orderImg = props.imagePosition === 'left' ? 'order-lg-1' : 'order-lg-2';
+    const orderText = props.imagePosition === 'left' ? 'order-lg-2' : 'order-lg-1';
+    
     return (
         <section id="scroll">
             <div className="container px-5">
                 <div className="row gx-5 align-items-center">
-                    <div className="col-lg-6 order-lg-2">
+                    <div className={`col-lg-6 ${orderImg}`}>
                         <div className="p-5"><img className="img-fluid rounded-circle" src={props.img} alt="..." /></div>
                     </div>
-                    <div className="col-lg-6 order-lg-1">
+                    <div className={`col-lg-6 ${orderText}`}>
                         <div className="p-5">
-                            <h2 className="display-4">For those about to rock...</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                            <h2 className="display-4">{props.title}</h2>
+                            <p>{props.detail}</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-
     )
 }
